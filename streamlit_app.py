c590ee37dbc594491eaf3e1c7705f0eb33b5a9da
NathanaelCooper <155431702+NathanaelCooper@users.noreply.github.com>
2025-03-04 23:55:45 +0000

    Worked

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from Views import FeedView, AddPostView
-from Views.AddPostView import AddPostView  # ✅ Import the class/function directly
+from Views.AddPostView import AddPostView  
 from Views.FeedView import FeedView
 from Services import get_feed, add_post
 AddPostView(add_post)
